40a799ed7eb4a940639268786628b5d5848605ba
Diego Santiesteban <gatodeveloper@gmail.com>
2016-10-12 23:33:17 -0500

    Added slider and style contact form

--- header.php
+++ header.php
@@ -3,11 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="shortcut icon" href="<?php echo get_template_directory_uri();?>/img/favicon.ico" />
+    <link rel="shortcut icon" href="<?php echo get_template_directory_uri();?>/img/favicon.png" />
     <title><?php wp_title(''); ?></title>
     <style type="text/css">
       @import url(<?php bloginfo("stylesheet_url");?>);
     </style>
+    <link href="<?php echo get_template_directory_uri();?>/content/bower_components/bxslider-4/dist/jquery.bxslider.min.css" rel="stylesheet" />
+    <script src="<?php echo get_template_directory_uri();?>/content/bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="<?php echo get_template_directory_uri();?>/content/bower_components/bxslider-4/dist/jquery.bxslider.min.js"></script>
+    <script src="<?php echo get_template_directory_uri();?>/js/main.js"></script>
     <?php add_theme_support("automatic-feed-links"); ?>
     <?php wp_head();?>
     <script>
--- page-work-experience.php
+++ page-work-experience.php
@@ -15,11 +15,28 @@
     <?php endif;?>
   </div>
 
-  <div class="slider-works shadow-separator">
+  <section class="slider-works-section shadow-separator">
     <div class="main-wrap">
-      
+      <ul class="slider-works bxslider">
+        <li>
+          <h3>TM Latin America: Digital Marketing Manager</h3>
+          <p class="time">November 2013 - Currently</p>
+
+          <p>As Social Media Chief Consultant and head of Digital Marketing, I'm in charge of creating effective and innovative digital marketing solutions for tourism marketing within the Latin American markets so as to maximize the return on investment for our customers.</p>
+
+          <p>Some of my tasks include launch and monitoring of Facebook Ads and Google AdWords campaigns, development of e-mail marketing campaigns, occasional Graphic Design work, editing and promotion of corporate blogs and prepare Digital Marketing proposals for current and new clients.</p>
+        </li>
+        <li>
+          <h3>TM Latin America: Digital Marketing Manager</h3>
+          <p class="time">November 2013 - Currently</p>
+
+          <p>As Social Media Chief Consultant and head of Digital Marketing, I'm in charge of creating effective and innovative digital marketing solutions for tourism marketing within the Latin American markets so as to maximize the return on investment for our customers.</p>
+
+          <p>Some of my tasks include launch and monitoring of Facebook Ads and Google AdWords campaigns, development of e-mail marketing campaigns, occasional Graphic Design work, editing and promotion of corporate blogs and prepare Digital Marketing proposals for current and new clients.</p>
+        </li>
+      </ul>
     </div>
-  </div>
+  </section>
 </section>
 <?php
   get_footer();
